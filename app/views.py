5345de6f490fd417fa6784fb373e15395a3cbb80
saucrio <ionut.sauciuc2003@gmail.com>
2022-09-27 17:25:30 +0300

    getting uploaded file and checking content type

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
+import os
 from django.shortcuts import render
 from django.http.response import HttpResponseNotAllowed, JsonResponse
 from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import MultiPartParser
 from rest_framework import generics, status
+from sharyx.settings import CONFIG, UPLOADS_PATH
 
 class upload(generics.ListCreateAPIView):
     """
@@ -10,9 +13,18 @@ class upload(generics.ListCreateAPIView):
     throttle_classes = []
     """
     renderer_classes = [JSONRenderer]
+    parser_classes = [MultiPartParser]
 
     def get(self, request):
         return JsonResponse({"detail":"Method POST not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def post(self, request):
-        pass
+        uploaded_file = request.data['file']
+        
+        # checking if the uploaded_file has a supported content type
+        if uploaded_file.content_type not in CONFIG['ALLOWED_CONTENT_TYPES']:
+            return JsonResponse({"detail":"content type not allowed."}, status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+        # saving the uploaded_file
+
+        return JsonResponse({"detail":"success."})
