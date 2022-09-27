cb4b06836aedfd994ae9614821f86d29599c4a59
saucrio <ionut.sauciuc2003@gmail.com>
2022-09-27 15:43:07 +0300

    updated settings and added basic upload endpoint

--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseNotAllowed, JsonResponse
+from rest_framework.renderers import JSONRenderer
+from rest_framework import generics, status
 
-# Create your views here.
+class upload(generics.ListCreateAPIView):
+    """
+    authentication_classes = []
+    permission_classes = []
+    throttle_classes = []
+    """
+    renderer_classes = [JSONRenderer]
+
+    def get(self, request):
+        return JsonResponse({"detail":"Method POST not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def post(self, request):
+        pass
--- sharyx/urls.py
+++ sharyx/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('upload/', views.upload.as_view(), name="upload"),
+
 ]
