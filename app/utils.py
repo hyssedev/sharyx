58f9a31380aba14a6a11f7dfca06b01001c07937
saucrio <ionut.sauciuc2003@gmail.com>
2022-09-28 19:35:56 +0300

    added file size check, updated config and moved upload logging

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,7 @@
 import uuid, os
 from sharyx.settings import UPLOADS_PATH
 from app.models import Uploads
+from sharyx.settings import logger
 
 def rename_upload(file):
     # TODO: maybe change uuid in the future
@@ -12,6 +13,6 @@ def rename_upload(file):
     # checking if there is any file with the name we are about to use for renaming, in the uploads directory, if there is not, rename and upload
     if not os.path.isfile(path):
         file.name = new_name
-        return Uploads.objects.create(name=new_name, uploaded_file=file), file.name, round(os.path.getsize(path) / (1024 * 1024), 2)
+        return Uploads.objects.create(name=new_name, uploaded_file=file), file.name, logger.info(f"uploaded file. name={file.name}, size={round(os.path.getsize(path) / (1024 * 1024), 2)} MB")
     else:
         rename_upload(file)
--- app/views.py
+++ app/views.py
@@ -6,7 +6,6 @@ from rest_framework.parsers import MultiPartParser
 from rest_framework import generics, status
 from sharyx.settings import CONFIG, UPLOADS_PATH
 from app.utils import rename_upload
-from sharyx.settings import logger
 
 class upload(generics.ListCreateAPIView):
     # TODO crypt it, hash it or smth
@@ -23,15 +22,14 @@ class upload(generics.ListCreateAPIView):
 
     def post(self, request):
         uploaded_file = request.data['file']
-        
+
+        # checking if the size of the uploaded_file is under 50MB
+        if uploaded_file.size > CONFIG['MAX_FILE_SIZE']: return JsonResponse({"detailt":"file size too large."}, status=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE)
         # checking if the uploaded_file has a supported content type
-        if uploaded_file.content_type not in CONFIG['ALLOWED_CONTENT_TYPES']:
-            return JsonResponse({"detail":"content type not allowed."}, status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+        if uploaded_file.content_type not in CONFIG['ALLOWED_CONTENT_TYPES']: return JsonResponse({"detail":"content type not allowed."}, status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
         # saving the uploaded_file, giving it a random name and checking if it already exists or not, and also saving the path
         file = rename_upload(uploaded_file)
-
-        logger.info(f"uploaded file. name={file[1]}, size={file[2]} MB")
 
         # returning the url for the uploaded_file
         return JsonResponse({"url":f"{request.scheme}://{request.get_host()}/uploads/{file[1]}"})
