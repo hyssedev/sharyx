f7cc69f065b001ce00e3e93449ab6d7fabd58821
saucrio <ionut.sauciuc2003@gmail.com>
2022-10-04 17:18:13 +0300

    function rename

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,9 @@
-import uuid, os
+import uuid, os, secrets
 from sharyx.settings import UPLOADS_PATH
 from app.models import Uploads
 from sharyx.settings import logger
 
-def rename_upload(file):
+def upload_file(file):
     # TODO: maybe change uuid in the future
     # new_name is renamed to new uuid4 hex, and we are also giving it the old extension
     new_name = f"{uuid.uuid4().hex}.{file.name.split('.').pop()}"
@@ -15,4 +15,4 @@ def rename_upload(file):
         file.name = new_name
         return Uploads.objects.create(name=new_name, uploaded_file=file), file.name, logger.info(f"uploaded file. name={file.name}, size={round(os.path.getsize(path) / (1024 * 1024), 2)} MB")
     else:
-        rename_upload(file)
+        upload_file(file)
--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import MultiPartParser
 from rest_framework import generics, status
 from sharyx.settings import CONFIG, UPLOADS_PATH
-from app.utils import rename_upload
+from app.utils import upload_file
 
 class upload(generics.ListCreateAPIView):
     # TODO crypt it, hash it or smth
@@ -30,7 +30,7 @@ class upload(generics.ListCreateAPIView):
         if uploaded_file.content_type not in CONFIG['ALLOWED_CONTENT_TYPES']: return JsonResponse({"detail":"content type not allowed."}, status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
         # saving the uploaded_file, giving it a random name and checking if it already exists or not, and also saving the path
-        file = rename_upload(uploaded_file)
+        file = upload_file(uploaded_file)
 
         # returning the url for the uploaded_file
         return JsonResponse({"url":f"{request.scheme}://{request.get_host()}/uploads/{file[1]}"})
