2d40787526071b8df0bdad50217c57db5163719a
saucrio <ionut.sauciuc2003@gmail.com>
2022-10-04 18:30:07 +0300

    small urlsafe token edit

--- app/utils.py
+++ app/utils.py
@@ -9,10 +9,11 @@ def upload_file(file):
     new_name = f"{uuid.uuid4().hex}.{file.name.split('.').pop()}"
 
     path = os.path.join(UPLOADS_PATH, new_name)
+    del_code = secrets.token_urlsafe(10)
 
     # checking if there is any file with the name we are about to use for renaming, in the uploads directory, if there is not, rename and upload
     if not os.path.isfile(path):
         file.name = new_name
-        return Uploads.objects.create(name=new_name, uploaded_file=file), file.name, logger.info(f"uploaded file. name={file.name}, size={round(os.path.getsize(path) / (1024 * 1024), 2)} MB")
+        return Uploads.objects.create(name=new_name, uploaded_file=file, delete_code=del_code), file.name, logger.info(f"uploaded file. name={file.name}, size={round(os.path.getsize(path) / (1024 * 1024), 2)} MB"), del_code
     else:
         upload_file(file)
