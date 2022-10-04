15103b65ad04aaeb5eb8896c7f11883f4caed554
saucrio <ionut.sauciuc2003@gmail.com>
2022-10-04 17:18:36 +0300

    updated model

--- app/models.py
+++ app/models.py
@@ -8,3 +8,4 @@ class Uploads(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=50)
     uploaded_file = models.FileField(upload_to=UPLOADS_DIR)
+    delete_code = models.CharField(max_length=6)
